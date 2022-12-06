c6b9cbe8c95473d80933423bcfd950d3606c04ba
Coderfrog256 <johnnie.bluntnose@gmail.com>
2022-12-05 21:28:58 -0800

    Remove duplicate code.

--- 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day6.kt
+++ 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day6.kt
@@ -3,18 +3,14 @@ package zone.frog.advent.twentytwo
 import java.io.File
 
 object Day6 {
-    private fun allUnique(chars: String) =
-        chars.toSet().size == chars.length
+    private fun allUnique(chars: String) = chars.toSet().size == chars.length
 
-    fun scenarioOne(textFile: String) =
-        File(textFile).readText()
-            .windowed(4, 1).withIndex()
-            .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars.value) } }
-            .let { it + 4 }
+    private fun firstUnique(textFile: String, groupSize: Int) = File(textFile).readText()
+        .windowed(groupSize, 1).withIndex()
+        .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars.value) } }
+        .let { it + groupSize }
 
-    fun scenarioTwo(textFile: String) =
-        File(textFile).readText()
-            .windowed(14, 1).withIndex()
-            .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars.value) } }
-            .let { it + 14 }
+    fun scenarioOne(textFile: String) = firstUnique(textFile, 4)
+
+    fun scenarioTwo(textFile: String) = firstUnique(textFile, 14)
 }
