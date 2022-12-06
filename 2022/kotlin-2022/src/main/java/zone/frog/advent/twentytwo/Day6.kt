381344d12fdb704c645afad6c357fdfbadf08601
Coderfrog256 <johnnie.bluntnose@gmail.com>
2022-12-05 21:20:01 -0800

    Small cleanup.

--- 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day6.kt
+++ 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day6.kt
@@ -1,21 +1,20 @@
 package zone.frog.advent.twentytwo
 
 import java.io.File
-import java.util.regex.Pattern
 
 object Day6 {
-    private fun allUnique(indexedChars: IndexedValue<String>) =
-        indexedChars.value.toSet().size == indexedChars.value.length
+    private fun allUnique(chars: String) =
+        chars.toSet().size == chars.length
 
     fun scenarioOne(textFile: String) =
         File(textFile).readText()
             .windowed(4, 1).withIndex()
-            .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars) } }
+            .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars.value) } }
             .let { it + 4 }
 
     fun scenarioTwo(textFile: String) =
         File(textFile).readText()
             .windowed(14, 1).withIndex()
-            .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars) } }
+            .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars.value) } }
             .let { it + 14 }
 }
