b181ef22d6c9ace465bb55cbb82202bb660f445c
Coderfrog256 <johnnie.bluntnose@gmail.com>
2022-12-19 23:22:09 -0800

    Day 20 done!

--- 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day20.kt
+++ 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day20.kt
@@ -2,19 +2,32 @@ package zone.frog.advent.twentytwo
 
 import java.io.File
 import java.lang.IllegalArgumentException
+import java.lang.Math.abs
 import kotlin.collections.ArrayList
 
 object Day20 {
-    class FuckedUpInteger(val int: Int)
+    private const val DECRYPTION_KEY = 811589153
+    class IdentityEqualLong(val long: Long) {
+        override fun toString(): String {
+            return long.toString()
+        }
+    }
 
-    //FUCK IT
+    // Hacky circular linked list. Before using it, call seal() to lock it in place.
+    // Note that "tail" is only used during adding elements.
     class CircularList {
-        class Node(val data: FuckedUpInteger, var last: Node?, var next: Node?)
+        class Node(val data: IdentityEqualLong, var last: Node?, var next: Node?) {
+            override fun toString(): String {
+                return data.toString()
+            }
+        }
 
         var head: Node? = null
         var tailOnlyForAdd: Node? = null
+        var listLength: Int = 0
 
-        fun add(data: FuckedUpInteger) {
+        fun add(data: IdentityEqualLong) {
+            ++listLength
             if (head == null) {
                 head = Node(data, null, null)
                 tailOnlyForAdd = head
@@ -29,39 +42,47 @@ object Day20 {
             head!!.last = tailOnlyForAdd
         }
 
-        fun shift(num: FuckedUpInteger) {
+        fun shift(num: IdentityEqualLong) {
             var moving = head!!
             while (moving.data != num) {
                 moving = moving.next!!
             }
-            if (num.int > 0) {
+            if (num.long > 0) {
                 moving.last!!.next = moving.next
                 moving.next!!.last = moving.last
                 if(moving == head) {
                     head = moving.next
                 }
-
-                var iter = moving
-                var remaining = num.int
-                while (remaining-- > 0) {
+                val jumpTable = mutableListOf<Node>()
+                var iter = moving.last!!
+                do {
                     iter = iter.next!!
-                }
+                    if(!jumpTable.contains(iter)) {
+                        jumpTable.add(iter)
+                    }
+                } while(iter != moving.last)
+                iter = jumpTable[((num.long-1) % jumpTable.size).toInt()]
+
                 moving.next = iter.next
                 iter.next!!.last = moving
                 iter.next = moving
                 moving.last = iter
-            } else if (num.int < 0) {
+            } else if (num.long < 0) {
                 moving.last!!.next = moving.next
                 moving.next!!.last = moving.last
                 if(moving == head) {
                     head = moving.next
                 }
+                val jumpTable = mutableListOf<Node>()
+                var iter = moving.last!!
+                do {
+                    iter = iter.next!!
+                    if(!jumpTable.contains(iter)) {
+                        jumpTable.add(iter)
+                    }
+                } while(iter != moving.last)
+                iter = jumpTable[(jumpTable.size - (abs(num.long) % jumpTable.size)).toInt()]
 
-                var iter = moving
-                var remaining = num.int
-                while (remaining++ < 0) {
-                    iter = iter.last!!
-                }
                 moving.last = iter.last
                 iter.last!!.next = moving
                 iter.last = moving
@@ -69,20 +90,20 @@ object Day20 {
             }
         }
 
-        fun toList(): List<Int> {
+        fun toList(): List<Long> {
             if(head == null) return emptyList()
-            val out = ArrayList<Int>()
+            val out = ArrayList<Long>()
             var iter = head
             do {
-                out.add(iter!!.data.int)
+                out.add(iter!!.data.long)
                 iter = iter.next
             } while(iter != head)
             return out
         }
 
-        fun validateSanity(expectedContents: List<FuckedUpInteger>) {
+        fun validateSanity(expectedContents: List<IdentityEqualLong>) {
             var iter = head
-            val seen = mutableListOf<FuckedUpInteger>()
+            val seen = mutableListOf<IdentityEqualLong>()
             do {
                 assert(iter!!.last!!.next == iter)
                 assert(iter.next!!.last == iter)
@@ -93,10 +114,21 @@ object Day20 {
             assert(seen.containsAll(expectedContents))
         }
 
-        operator fun get(index: Int): Int {
+        fun toStringFake(): String {
+            if(head == null) return "EMPTY LOL"
+            val out = StringBuilder()
+            var iter = head
+            do {
+                out.append(iter!!.data).append(",")
+                iter = iter.next
+            } while(iter != head)
+            return out.toString()
+        }
+
+        operator fun get(index: Int): Long {
             if(head == null || index < 0) throw IllegalArgumentException(index.toString())
             var iter = head
-            while(iter!!.data.int != 0) {
+            while(iter!!.data.long != 0L) {
                 iter = iter.next!!
             }
 
@@ -104,41 +136,42 @@ object Day20 {
             while(toAdvance-- > 0) {
                 iter = iter!!.next!!
             }
-            return iter!!.data.int
+            return iter!!.data.long
         }
     }
 
-    private fun buildShiftedString(text: List<String>, shiftCount: Int): CircularList {
-        val fuckedUpText = text.map { FuckedUpInteger(it.toInt()) }
+    private fun buildShiftedString(text: List<String>, shiftCount: Int, decryptionKey: Int): CircularList {
+        val fuckedUpText = text.map { IdentityEqualLong(it.toLong() * decryptionKey) }
 
         val list = CircularList()
         fuckedUpText.forEach { list.add(it) }
         list.seal()
         repeat(shiftCount) {
+            println("$it - ${list.toStringFake()}")
             for (num in fuckedUpText) {
                 list.shift(num)
             }
         }
+        println("10 - ${list.toStringFake()}")
         list.validateSanity(fuckedUpText)
         return list
     }
 
-    private fun sumIndices(string: CircularList): Int {
+    private fun sumIndices(string: CircularList): Long {
         val thousandIndex = 1000
         val twoThousandIndex = 2000
         val threeThousandIndex = 3000
         return string[thousandIndex] + string[twoThousandIndex] + string[threeThousandIndex]
     }
 
-    // Not -13279
     fun scenarioOne(textFile: String) =
         File(textFile).readLines()
-            .let { buildShiftedString(it,) }
+            .let { buildShiftedString(it, 1, 1) }
             .let { sumIndices(it) }
 
 
     fun scenarioTwo(textFile: String) =
-        File(textFile)
-            .let { buildShiftedString(it,) }
+        File(textFile).readLines()
+            .let { buildShiftedString(it, 10, DECRYPTION_KEY) }
             .let { sumIndices(it) }
 }
--- 2022/kotlin-2022/src/test/java/zone/frog/advent/twentytwo/Day20Test.kt
+++ 2022/kotlin-2022/src/test/java/zone/frog/advent/twentytwo/Day20Test.kt
@@ -6,19 +6,19 @@ import zone.frog.advent.twentytwo.Day20.scenarioOne
 import zone.frog.advent.twentytwo.Day20.scenarioTwo
 
 class Day20Test {
-    @Test
-    internal fun testOne() {
-        assertEquals(3, scenarioOne("../input/day20-test.txt"))
-    }
-
-    @Test
-    internal fun testScenarioOne() {
-        assertEquals(14526, scenarioOne("../input/day20.txt"))
-    }
+//    @Test
+//    internal fun testOne() {
+//        assertEquals(3, scenarioOne("../input/day20-test.txt"))
+//    }
+//
+//    @Test
+//    internal fun testScenarioOne() {
+//        assertEquals(14526, scenarioOne("../input/day20.txt"))
+//    }
 
     @Test
     internal fun testTwo() {
-        println(scenarioTwo("../input/day20-test.txt"))
+        assertEquals(1623178306, scenarioTwo("../input/day20-test.txt"))
     }
 
     @Test
