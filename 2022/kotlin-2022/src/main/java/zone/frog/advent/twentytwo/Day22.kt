a162588eb7e26a5091f4f1a35025351ae5446946
Coderfrog256 <johnnie.bluntnose@gmail.com>
2022-12-21 22:19:07 -0800

    WE HAVE CUBE DETECTION.

--- 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day22.kt
+++ 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day22.kt
@@ -2,7 +2,6 @@ package zone.frog.advent.twentytwo
 
 import java.io.File
 import java.lang.IllegalArgumentException
-import java.text.FieldPosition
 
 
 object Day22 {
@@ -19,7 +18,7 @@ object Day22 {
         LEFT(-1 to 0, 2);
 
         fun spin(direction: String): Direction {
-            if(direction == "Z") return this
+            if (direction == "Z") return this
             return when (direction to this) {
                 "R" to UP -> RIGHT
                 "R" to RIGHT -> DOWN
@@ -62,6 +61,51 @@ object Day22 {
         return grid to instructions
     }
 
+    private fun parseMapCubeAndDirections(
+        text: List<String>,
+        cubeSideSize: Int
+    ): Pair<Map<IntPair, TileType>, List<Instruction>> {
+        val grid = mutableMapOf<IntPair, TileType>()
+        for (y in 0 until text.size - 2) {
+            for (x in 0 until text[y].length) {
+                when (text[y][x]) {
+                    // Final answer is 1-indexed. We start at one!
+                    '.' -> grid[x + 1 to y + 1] = TileType.EMPTY
+                    '#' -> grid[x + 1 to y + 1] = TileType.WALL
+                }
+            }
+        }
+        //Determine ranges for each side.
+        var sides = mutableListOf<Pair<IntRange, IntRange>>()
+        var i = 1
+        while (i < grid.maxOf { it.key.second }) {
+            val line = grid.filter { it.key.second == i }
+            val lineStart = line.minOf { it.key.first }
+            val lineEnd = line.maxOf { it.key.first }
+
+            for (j in 0..(lineEnd - lineStart) / cubeSideSize) {
+                sides += (lineStart + (cubeSideSize*j)) until (lineStart + (cubeSideSize * (j + 1))) to i..i + (cubeSideSize - 1)
+            }
+            i += cubeSideSize
+        }
+
+        val lineOne = grid.filter { it.key.second == 1 }
+        val sideOneStart = lineOne.minOf { it.key.second }
+
+
+        val instructionString = text.last() + "Z"
+        val commandRegex = Regex("(\\d+)(\\w)")
+
+        val instructions = mutableListOf<Instruction>()
+        i = 0
+        while (i < instructionString.length) {
+            val match = commandRegex.matchAt(instructionString, i) ?: break
+            i += match.value.length
+            instructions += Instruction(match.groupValues[1].toInt(), match.groupValues[2])
+        }
+        return grid to instructions
+    }
+
     private fun step(
         playerPosition: IntPair,
         playerDirection: Direction,
@@ -70,11 +114,37 @@ object Day22 {
     ): IntPair {
         return when (grid[newPosition]) {
             null -> when (playerDirection) {
-                    Direction.UP -> step(playerPosition, playerDirection, playerPosition.first to grid.filter { it.key.first == playerPosition.first }.maxOf { it.key.second }, grid)
-                    Direction.RIGHT -> step(playerPosition, playerDirection,  grid.filter { it.key.second == playerPosition.second }.minOf { it.key.first } to playerPosition.second, grid)
-                    Direction.DOWN -> step(playerPosition, playerDirection, playerPosition.first to grid.filter { it.key.first == playerPosition.first }.minOf { it.key.second }, grid)
-                    Direction.LEFT -> step(playerPosition, playerDirection,  grid.filter { it.key.second == playerPosition.second }.maxOf { it.key.first } to playerPosition.second, grid)
-                }
+                Direction.UP -> step(
+                    playerPosition,
+                    playerDirection,
+                    playerPosition.first to grid.filter { it.key.first == playerPosition.first }
+                        .maxOf { it.key.second },
+                    grid
+                )
+
+                Direction.RIGHT -> step(
+                    playerPosition,
+                    playerDirection,
+                    grid.filter { it.key.second == playerPosition.second }
+                        .minOf { it.key.first } to playerPosition.second,
+                    grid)
+
+                Direction.DOWN -> step(
+                    playerPosition,
+                    playerDirection,
+                    playerPosition.first to grid.filter { it.key.first == playerPosition.first }
+                        .minOf { it.key.second },
+                    grid
+                )
+
+                Direction.LEFT -> step(
+                    playerPosition,
+                    playerDirection,
+                    grid.filter { it.key.second == playerPosition.second }
+                        .maxOf { it.key.first } to playerPosition.second,
+                    grid)
+            }
+
             TileType.EMPTY -> newPosition
             TileType.WALL -> playerPosition
         }
@@ -98,7 +168,7 @@ object Day22 {
             .let { parseMapAndDirections(it) }
             .let { getCoordinateValue(it.first, it.second) }
 
-    fun scenarioTwo(textFile: String) =
-        File(textFile)
-
+    fun scenarioTwo(textFile: String, cubeSideSize: Int) =
+        File(textFile).readLines()
+            .let { parseMapCubeAndDirections(it, cubeSideSize) }
 }
--- 2022/kotlin-2022/src/test/java/zone/frog/advent/twentytwo/Day22Test.kt
+++ 2022/kotlin-2022/src/test/java/zone/frog/advent/twentytwo/Day22Test.kt
@@ -6,23 +6,23 @@ import zone.frog.advent.twentytwo.Day22.scenarioOne
 import zone.frog.advent.twentytwo.Day22.scenarioTwo
 
 class Day22Test {
-    @Test
-    internal fun testOne() {
-        println(scenarioOne("../input/day22-test.txt"))
-    }
-
-    @Test
-    internal fun testScenarioOne() {
-        println(scenarioOne("../input/day22.txt"))
-    }
+//    @Test
+//    internal fun testOne() {
+//        assertEquals(6032, scenarioOne("../input/day22-test.txt"))
+//    }
+//
+//    @Test
+//    internal fun testScenarioOne() {
+//        assertEquals(77318, scenarioOne("../input/day22.txt"))
+//    }
 
     @Test
     internal fun testTwo() {
-        println(scenarioTwo("../input/day22-test.txt"))
+        println(scenarioTwo("../input/day22-test.txt", 4))
     }
 
     @Test
     internal fun testScenarioTwo() {
-        println(scenarioTwo("../input/day22.txt"))
+        println(scenarioTwo("../input/day22.txt", 50))
     }
 }
