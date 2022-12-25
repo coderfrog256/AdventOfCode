c63dd76d7d228c489891f5aa67e6c44b86fb022a
Coderfrog256 <johnnie.bluntnose@gmail.com>
2022-12-24 21:25:58 -0800

    I guess this is TDD

---
+++ 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day25.kt
@@ -0,0 +1,21 @@
+package zone.frog.advent.twentytwo
+
+import java.io.File
+import java.lang.Math.abs
+
+
+object Day25 {
+    fun decimalToSnafu(decimal: Long): String {
+        return "fuck"
+    }
+
+    fun snafuToDecimal(snafu: String): Long {
+        return Int.MAX_VALUE.toLong()
+    }
+
+    fun scenarioOne(textFile: String) =
+        File(textFile)
+
+    fun scenarioTwo(textFile: String) =
+        File(textFile)
+}
