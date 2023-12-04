979df39d52d64596a2d9d725c6fe37e495322567
Coderfrog256 <johnnie.bluntnose@gmail.com>
2023-12-03 22:10:59 -0800

    2023 Day 4 in Lisp.

--- 2023/lisp/bak.lisp
+++ 2023/lisp/bak.lisp
@@ -1,43 +1,63 @@
 (ql:quickload '(:str :cl-ppcre :binding-arrows :hu.dwim.defclass-star :alexandria :parseq :metabang-bind :fset :priority-queue))
 (defpackage :advent (:use :cl :cl-ppcre :binding-arrows :parseq :metabang-bind :hu.dwim.defclass-star))
 (in-package :advent)
+(declaim (optimize (speed 3) (safety 0) (debug 0) (compilation-speed 0)))
+(defun get-matches (line)
+  (let ((winning (mapcar #'parse-integer
+                         (remove-if (lambda (x) (= 0 (length x)))
+                                    (str:split " "
+                                               (first (str:split "|" (second (str:split ": " line))))))))
+        (hand (mapcar #'parse-integer
+                      (remove-if (lambda (x) (= 0 (length x)))
+                                 (str:split " "
+                                            (second (str:split "|" (second (str:split ": " line)))))))))
+    (loop with win-count = 0 and out = 0
+          for num in hand
+          when (member num winning)
+            do (incf win-count)
+          finally (return
+                    (if (= win-count 0)
+                        0
+                        (expt 2 (1- win-count)))))))
 
 (defun part-1 (file)
-  (let ((claimed (make-hash-table :test #'equal))
-        (cleaned (str:replace-all "." " " file)))
-    (loop for line in (str:lines file)
-          for y from 0
-          do (loop for char across line
-                   for x from 0
-                   when (and (not (equal char #\.)) (not (digit-char-p char))
-                             (setf (gethash (list (1- x) y) claimed) t)
-                             (setf (gethash (list (1+ x) y) claimed) t)
-                             (setf (gethash (list x (1- y)) claimed) t)
-                             (setf (gethash (list x (1+ y)) claimed) t)
-                             (setf (gethash (list (1+ x) (1+ y)) claimed) t)
-                             (setf (gethash (list (1- x) (1+ y)) claimed) t)
-                             (setf (gethash (list (1+ x) (1- y)) claimed) t))
-                     do (setf (gethash (list (1- x) (1- y)) claimed) t)))
-    (loop with valid = (list)
-          for line in (str:lines file)
-          for y from 0
-          do (loop with working-number = (list)
-                   with num-valid = nil
-                   for char across line
-                   for x from 0
-                   do (if (digit-char-p char)
-                          (progn
-                            (push char working-number)
-                            (format t "Space valid: (~a, ~a) ~a (was ~a)~%" x y (gethash (list x y) claimed nil) num-valid)
-                            (setf num-valid (or num-valid (gethash (list x y) claimed nil))))
-                          (progn
-                            (when (and num-valid working-number)
-                              (push (parse-integer (coerce (nreverse working-number) 'string)) valid))
-                            (setf num-valid nil
-                                  working-number nil)))
-                   finally (when (and num-valid working-number)
-                             (push (parse-integer (coerce (nreverse working-number) 'string)) valid)))
-          finally (return (reduce #'+ valid)))))
+  (loop for line in (str:lines file)
+        summing (get-matches line)))
 
-(print (time (part-1
- (frog:get-advent-of-code-input 2023 3))))
+(print (part-1
+        (frog:get-advent-of-code-input 2023 4)))
+
+(defun get-match-count (line)
+  (let ((winning (mapcar #'parse-integer
+                         (remove-if (lambda (x) (= 0 (length x)))
+                                    (str:split " "
+                                               (first (str:split "|" (second (str:split ": " line))))))))
+        (hand (mapcar #'parse-integer
+                      (remove-if (lambda (x) (= 0 (length x)))
+                                 (str:split " "
+                                            (second (str:split "|" (second (str:split ": " line)))))))))
+    (loop with win-count = 0 and out = 0
+          for num in hand
+          when (member num winning)
+            do (incf win-count)
+          finally (return win-count))))
+
+(defun part-2 (file)
+  (let* ((lines (str:lines file))
+         (pile (make-hash-table)))
+    (loop for i upto (length lines)
+          do (setf (gethash i pile) 1))
+    (loop with cards-played = 0
+          for card from 0 below (length lines)
+          for plays = (gethash card pile)
+          for wins = (get-match-count (nth card lines))
+          do (incf cards-played plays)
+             (loop for win below wins
+                   do (incf (gethash (+ 1 card win) pile) plays))
+          finally (return cards-played))))
+
+
+(print
+(time
+(part-2
+ (frog:get-advent-of-code-input 2023 4))))
