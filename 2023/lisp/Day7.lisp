30ecd47b1c048ce33bfae4858a9a48c698f99f09
Coderfrog256 <johnnie.bluntnose@gmail.com>
2023-12-07 00:24:59 -0800

    Cleaned up.

--- 2023/lisp/Day7.lisp
+++ 2023/lisp/Day7.lisp
@@ -1,75 +1,58 @@
-(ql:quickload '(:str :cl-ppcre :binding-arrows :hu.dwim.defclass-star :alexandria :parseq :metabang-bind :fset :priority-queue))
-(defpackage :advent (:use :cl :cl-ppcre :binding-arrows :parseq :metabang-bind :hu.dwim.defclass-star))
+(ql:quickload '(:str :binding-arrows :alexandria))
+(defpackage :advent (:use :cl :binding-arrows))
 (in-package :advent)
-
-(defparameter *joker-world* nil)
-(defparameter *card-ordering* '()) ; Set by solve method
-
+(defconstant +joker+ #\J)
+(defparameter *joker-world* nil)   ; Differs based on part
+(defparameter *card-ordering* '())
 (defun count-chars (list)
   (loop with counts = (make-hash-table)
-        for char in (if *joker-world* (remove #\J list) list)
+        for char in (if *joker-world* (remove +joker+ list) list)
         do (incf (gethash char counts 0))
         finally (return counts)))
 
 (defun five-of-a-kind (hand)
-  (if (and *joker-world* (member #\J hand))
-      (if (= 5 (count #\J hand)) t
-          (let ((counts (count-chars hand)))
-            (= 5 (+ (count #\J hand) (first (sort (alexandria:hash-table-values counts) #'>))))))
+  (if (and *joker-world* (member +joker+ hand))
+      (if (= 5 (count +joker+ hand)) t
+          (= 5 (+ (count +joker+ hand) (first (sort (alexandria:hash-table-values (count-chars hand)) #'>)))))
       (every (lambda (card) (equal card (first hand))) hand)))
 
-(let ((*joker-world* t)) (five-of-a-kind (coerce "AAAAJ" 'list)))
-
 (defun four-of-a-kind (hand)
-  (if (and *joker-world* (member #\J hand))
-      (= 4 (+ (count #\J hand) (first (sort (alexandria:hash-table-values (count-chars hand)) #'>))))
+  (if (and *joker-world* (member +joker+ hand))
+      (= 4 (+ (count +joker+ hand) (first (sort (alexandria:hash-table-values (count-chars hand)) #'>))))
       (= 4 (first (sort (alexandria:hash-table-values (count-chars hand)) #'>)))))
 
 (defun full-house (hand)
-  (if (and *joker-world* (member #\J hand))
+  (if (and *joker-world* (member +joker+ hand))
       (let ((counts (alexandria:hash-table-values (count-chars hand))))
-        (ecase (count #\J hand)
-          (4 t)
-          (3 t)
+        (case (count +joker+ hand)
           (2 (<= 2 (first (sort counts #'>))))
           (1 (or (= 3 (first (sort counts #'>)))
                  (and (= 2 (first (sort counts #'>)))
-                      (= (first (sort counts #'>)) (second (sort counts #'>))))))))
+                      (= (first (sort counts #'>)) (second (sort counts #'>))))))
+          (t t)))
       (let ((counts (alexandria:hash-table-values (count-chars hand))))
-        (and (= 3 (first (sort counts #'>)))
-             (= 2 (second (sort counts #'>)))))))
-
-(let ((*joker-world* t)) (full-house (coerce "26J395" 'list)))
+        (and (= 3 (first (sort counts #'>))) (= 2 (second (sort counts #'>)))))))
 
 (defun three-of-a-kind (hand)
-  (if (and *joker-world* (member #\J hand))
-      (<= 3 (+ (count #\J hand) (first (sort (alexandria:hash-table-values (count-chars hand)) #'>))))
+  (if (and *joker-world* (member +joker+ hand))
+      (<= 3 (+ (count +joker+ hand) (first (sort (alexandria:hash-table-values (count-chars hand)) #'>))))
       (= 3 (first (sort (alexandria:hash-table-values (count-chars hand)) #'>)))))
 
-(let ((*joker-world* t)) (three-of-a-kind (coerce "ACBJJ" 'list)))
-
 (defun two-pair (hand)
-  (if (and *joker-world* (member #\J hand))
+  (if (and *joker-world* (member +joker+ hand))
       (let ((counts (alexandria:hash-table-values (count-chars hand))))
-        (ecase (count #\J hand)
-          (4 t)
-          (3 t)
-          (2 t)
-          (1 (= 2 (first (sort counts #'>))))))
+        (if (= 1 (count +joker+ hand)) ; If only one joker, then we need an existing pair
+            (= 2 (first (sort counts #'>)))
+            t)) ; If two or more, they can make their own pairs.
       (let ((counts (alexandria:hash-table-values (count-chars hand))))
         (and (= 2 (first (sort counts #'>)))
              (= 2 (second (sort counts #'>)))))))
 
-(let ((*joker-world* t)) (two-pair (coerce "ABBJA" 'list)))
-
-
 (defun one-pair (hand)
-  (if (and *joker-world* (member #\J hand)) t
-      (let ((counts (alexandria:hash-table-values (count-chars hand))))
-        (= 2 (first (sort counts #'>))))))
+  (if (and *joker-world* (member +joker+ hand)) t ; A joke can make anything a pair.
+      (= 2 (first (sort (alexandria:hash-table-values (count-chars hand)) #'>)))))
 
-(defun high-card (hand)
-  (every (lambda (count) (= 1 count)) (alexandria:hash-table-values (count-chars hand))))
+(defun high-card (hand) (every (lambda (count) (= 1 count)) (alexandria:hash-table-values (count-chars hand))))
 
 (defun classify-hand (hand)
   (cond ((five-of-a-kind hand) 7)
@@ -82,10 +65,10 @@
 
 (defun left-first (left right)
   (loop for i from 0 below (min (length left) (length right))
-        for left-card = (position (nth i left) *card-ordering*)
+        for left-card = (position (nth i left) *card-ordering*) ; Note: order is high to low.
         for right-card = (position (nth i right) *card-ordering*)
         when (< left-card right-card) return nil
-          when (> left-card right-card) return t))
+        when (> left-card right-card) return t))
 
 (defun compare-hands (left-pair right-pair)
   (let* ((left-hand (first left-pair))
@@ -101,30 +84,17 @@
     (list (coerce (first parts) 'list)
           (parse-integer (second parts)))))
 
-(defun part-1 (file)
-  (let* ((*joker-world* nil)
-         (*card-ordering* '(#\A #\K #\Q #\J #\T #\9 #\8 #\7 #\6 #\5 #\4 #\3 #\2))
-         (sorted (-<>> file
+(defun run-solution (file)
+  (let* ((sorted (-<>> file
                    (str:lines)
                    (mapcar #'parse-hand)
                    (sort <> #'compare-hands))))
-    (loop for i from 0 below (length sorted)
-          for hand = (nth i sorted)
-          sum (* (1+ i) (second hand)))))
+    (loop for i from 0 below (length sorted) sum (* (1+ i) (second (nth i sorted))))))
 
-(print (time (part-1 (frog:get-advent-of-code-input 2023 7 :input-suffix "test"))))
-(print (time (part-1 (frog:get-advent-of-code-input 2023 7))))
+(let ((*joker-world* nil) ; Not in joker world, jokers are regular cards.
+      (*card-ordering* '(#\A #\K #\Q +joker+ #\T #\9 #\8 #\7 #\6 #\5 #\4 #\3 #\2))) ; Regular order
+  (print (time (run-solution (frog:get-advent-of-code-input 2023 7))))) ; Part 1
 
-(defun part-2 (file)
-  (let* ((*joker-world* t)
-         (*card-ordering* '(#\A #\K #\Q #\T #\9 #\8 #\7 #\6 #\5 #\4 #\3 #\2 #\J))
-         (sorted (-<>> file
-                   (str:lines)
-                   (mapcar #'parse-hand)
-                   (sort <> #'compare-hands))))
-    (loop for i from 0 below (length sorted)
-          for hand = (nth i sorted)
-          sum (* (1+ i) (second hand)))))
-
-(print (time (part-2 (frog:get-advent-of-code-input 2023 7 :input-suffix "test"))))
-(print (time (part-2 (frog:get-advent-of-code-input 2023 7))))
+(let ((*joker-world* t) ; Joker world, enable shape shifting.
+      (*card-ordering* '(#\A #\K #\Q #\T #\9 #\8 #\7 #\6 #\5 #\4 #\3 #\2 +joker+))) ; Jokers are lowest.
+  (print (time (run-solution (frog:get-advent-of-code-input 2023 7))))) ; Part 2
