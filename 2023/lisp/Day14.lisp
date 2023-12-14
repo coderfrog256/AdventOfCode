54759682ac80cef1db95ad5743b251ba83d987bc
Coderfrog256 <johnnie.bluntnose@gmail.com>
2023-12-14 00:12:47 -0800

    Polish

--- 2023/lisp/Day14.lisp
+++ 2023/lisp/Day14.lisp
@@ -2,7 +2,7 @@
 (defpackage :advent (:use :cl :cl-ppcre :binding-arrows :parseq :metabang-bind :hu.dwim.defclass-star))
 (in-package :advent)
 (declaim (optimize (speed 3) (safety 0) (debug 0) (compilation-speed 0)))
-
+(defparameter *cave-cache* (make-instance 'function-cache:lru-cache :capacity 1000000))
 (defclass* cube () (x y))
 (defclass* rock () (x y))
 
@@ -35,33 +35,20 @@
               (reduce (if (equal direction :left) #'max #'min) <> :key #'x-of :initial-value (if (equal direction :left) -1 max-x))
               (+ (if (equal direction :left) 1 -1))))))
 
-(defun move-up (cave max-x max-y)
-  (loop for y from 0 below max-y
-        do (loop for x from 0 below max-x
-                 for rock = (gethash (list y x) cave)
-                 if (rockp rock) do (remhash (list y x) cave)
-                                    (setf (gethash (list (roll-vertical rock cave :up max-y) x) cave) rock))))
+(defmacro move (outer-iter inner-iter direction)
+  (let* ((roll (if (member direction '(:up :down)) 'roll-vertical 'roll-horizontal))
+         (max (if (member direction '(:up :down)) 'max-y 'max-x))
+         (update (if (member direction '(:up :down)) `(list (,roll rock cave ,direction ,max) x) `(list y (,roll rock cave ,direction ,max)))))
+    `(loop ,@outer-iter
+           do (loop ,@inner-iter
+                    for rock = (gethash (list y x) cave)
+                    if (rockp rock) do (remhash (list y x) cave)
+                                       (setf (gethash ,update cave) rock)))))
 
-(defun move-down (cave max-x max-y)
-  (loop for y from (1- max-y) downto 0
-        do (loop for x from 0 below max-x
-                 for rock = (gethash (list y x) cave)
-                 if (rockp rock) do (remhash (list y x) cave)
-                                    (setf (gethash (list (roll-vertical rock cave :down max-y) x) cave) rock))))
-
-(defun move-left (cave max-x max-y)
-  (loop for x from 0 below max-x
-        do (loop for y from 0 below max-y
-                 for rock = (gethash (list y x) cave)
-                 if (rockp rock) do (remhash (list y x) cave)
-                                    (setf (gethash (list y (roll-horizontal rock cave :left max-x)) cave) rock))))
-
-(defun move-right (cave max-x max-y)
-  (loop for x from (1- max-x) downto 0
-        do (loop for y from 0 below max-y
-                 for rock = (gethash (list y x) cave)
-                 if (rockp rock) do (remhash (list y x) cave)
-                                    (setf (gethash (list y (roll-horizontal rock cave :right max-x)) cave) rock))))
+(defun move-up (cave max-x max-y) (move (for y from 0 below max-y) (for x from 0 below max-x) :up))
+(defun move-down (cave max-x max-y) (move (for y from (1- max-y) downto 0) (for x from 0 below max-x) :down))
+(defun move-left (cave max-x max-y) (move (for x from 0 below max-x) (for y from 0 below max-y) :left))
+(defun move-right (cave max-x max-y) (move (for x from (1- max-x) downto 0) (for y from 0 below max-y) :right))
 
 (defun parse-cave (file)
   (loop with lines = (str:lines file)
@@ -75,9 +62,19 @@
                        finally (return x)) into max-x
         finally (return (list out max-x y))))
 
-;; Print the cave each cube is a #, each O is a rock, empty spaces are .
+(defun parse-cave (file)
+  (loop with lines = (str:lines file)
+        with out = (serapeum:dict)
+        for line in lines and y from 0
+        maximize (loop for x from 0 below (length line)
+                       for c = (char line x)
+                       if (char= c #\O) do (setf (gethash (list y x) out) (make-instance 'rock :x x :y y))
+                       else if (char= c #\#) do (setf (gethash (list y x) out) (make-instance 'cube :x x :y y))
+                       finally (return x)) into max-x
+        finally (return (list out max-x y))))
+
 (defun cave-string (cave max-x max-y)
-  (with-output-to-string (sb)
+  (intern (with-output-to-string (sb)
     (loop for y from 0 below max-y
           do (loop for x from 0 below max-x
                    for rock = (gethash (list y x) cave)
@@ -85,42 +82,34 @@
                    else if (cubep rock) do (format sb "#")
                    else do (format sb "."))
              (format sb "~%")
-          finally (format sb "~%"))))
+          finally (format sb "~%")))))
 
 (defun part-1 (file)
   (let* ((parsed (parse-cave file))
          (cave (first parsed))
-         (max-x (second parsed))
-         (max-y (third parsed)))
+         (max-x (second parsed)) (max-y (third parsed)))
     (move-up cave max-x max-y)
-    (reduce #'+ (remove-if-not #'rockp (alexandria:hash-table-values cave)) :key (lambda (r) (1+ (- max-y (y-of r) 1 ))))))
-(format t "~a~%" (time (part-1 (frog:get-advent-of-code-input 2023 14))))
+    (reduce #'+ (remove-if-not #'rockp (alexandria:hash-table-values cave))
+            :key (lambda (r) (1+ (- max-y (y-of r) 1 ))))))
+(print (time (part-1 (frog:get-advent-of-code-input 2023 14))))
 
-(defparameter *cave-cache* (make-instance 'function-cache:lru-cache :capacity 1000000))
 (defun cycle (cave key max-x max-y)
-  (let* ((cached (function-cache:get-cached-value *cave-cache* key))
-         new-key)
-    (if cached cached
-        (progn
-          (move-up cave max-x max-y)
-          (move-left cave max-x max-y)
-          (move-down cave max-x max-y)
-          (move-right cave max-x max-y)
-          (setf new-key (intern (cave-string cave max-x max-y)))
-          (setf (function-cache:get-cached-value *cave-cache* key) (list (copy-cave cave) new-key))
-          (list cave new-key)))))
+  (let* ((cached (function-cache:get-cached-value *cave-cache* key)) new-key)
+    (when cached (return-from cycle cached))
+    (mapc (lambda (f) (funcall f cave max-x max-y)) '(move-up move-left move-down move-right))
+    (setf new-key (cave-string cave max-x max-y)
+          (function-cache:get-cached-value *cave-cache* key) (list (copy-cave cave) new-key))
+    (list cave new-key)))
 
 (defun part-2 (file)
   (let* ((parsed (parse-cave file))
          (cave (first parsed))
-         (max-x (second parsed))
-         (max-y (third parsed))
-         (key (intern (cave-string cave max-x max-y))))
+         (max-x (second parsed)) (max-y (third parsed))
+         (key (cave-string cave max-x max-y)))
     (dotimes (i 1000000000)
-      (when (zerop (mod i 100000)) (format t "~a~%" i))
+      (when (zerop (mod i 100000)) (format t "~a~%" i)) ; Heartbeat
       (let ((cycled (cycle cave key max-x max-y)))
-        (setf cave (first cycled)
-              key (second cycled))))
+        (setf cave (first cycled) key (second cycled))))
     (reduce #'+ (remove-if-not #'rockp (alexandria:hash-table-values cave))
             :key (lambda (r) (1+ (- max-y (y-of r) 1 ))))))
-(format t "~a~%" (time (part-2 (frog:get-advent-of-code-input 2023 14))))
+(print (time (part-2 (frog:get-advent-of-code-input 2023 14))))
